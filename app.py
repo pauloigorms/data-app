a15edc06e5baec38b83c41b1b6fe397932c01a3f
Paulo Moraes <pauloigormoraes@gmail.com>
2021-09-03 21:24:50 -0400

    feature: edition on inputs and strings page

--- app.py
+++ app.py
@@ -7,25 +7,24 @@ url = 'https://ussouthcentral.services.azureml.net/workspaces/c660530bfcd7498c8d
 api_key = 'y8tkDLr3DiceKr2YrJoSe4R7SHUzqA0lrCr85rqRmyCOkjrUWIdn8yVnu2SD0drKubYEgSQ0n+sITKZoarrVHQ==' # Replace this with the API key for the web service
 headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
-st.title("Web Data App | Dataset Heart")
-st.markdown("Este é o projeto final para o módulo de Cloud e Infraestrutura para Ciência de Dados")
-
+st.title("Web Data App | Health care: Heart attack possibility")
+st.markdown("This is the final project for the Cloud and Infrastructure for Data Science module")
 
 try:
 
-    age = st.text_input("Age", value="1")
-    sex = st.text_input("Sex", value="1")
-    cp = st.text_input("Chest Tain Type", value="1")
-    trestbps = st.text_input("Testing Blood Pressure", value="1")
+    age = st.text_input("Age", value="27")
+    sex = st.text_input("Sex (1 = male; 0 = female)", value="1")
+    cp = st.text_input("Chest Tain Type (1: typical angina; 2: atypical angina; 3: non-anginal pain; 4: asymptomatic)", value="1")
+    trestbps = st.text_input("Resting Blood Pressure (in mm Hg on admission to the hospital)", value="1")
     chol = st.text_input("Serum Cholestoral in mg/dl", value="1")
-    fbs = st.text_input("Fasting Blood Sugar > 120 mg/dl", value="1")
-    restecg = st.text_input("Resting Electrocardiographic Results (values 0,1,2)", value="1")
-    thalach = st.text_input("Maximum Heart Rate Achieved", value="1")
-    exang = st.text_input("Exercise Induced Angina", value="1")
+    fbs = st.text_input("Fasting Blood Sugar (fasting blood sugar > 120 mg/dl) (1 = true; 0 = false)", value="1")
+    restecg = st.text_input("Resting Electrocardiographic Results (values 0 = normal, 1 = having ST-T wave abnormality, 2 = showing probable or definite left ventricular hypertrophy by Estes' criteria)", value="1")
+    thalach = st.text_input("Maximum Heart Rate Achieved (maximum heart rate achieved)", value="1")
+    exang = st.text_input("Exercise Induced Angina (1 = yes; 0 = no)", value="1")
     oldpeak = st.text_input("Oldpeak = ST Depression Induced by Exercise Relative to Rest", value="1")
     slope = st.text_input("The Slope of the Peak Exercise ST Segment", value="1")
     ca = st.text_input("Number of Major Vessels (0-3) Colored by Flourosopy", value="1")
-    thal = st.text_input("THAL: 0 = normal; 1 = fixed defect; 2 = reversable defect", value="1")
+    thal = st.text_input("THAL (3 = normal; 6 = fixed defect; 7 = reversable defect)", value="1")
     target = st.text_input("Target: 0= less chance of heart attack 1= more chance of heart attack", value="1")
 
     data = {
@@ -62,7 +61,7 @@ try:
         response = urllib.request.urlopen(req)
         result = response.read()
         data = ast.literal_eval(result.decode("UTF-8"))
-        st.write("Resultado: ", data['Results'])
+        st.write("Results prediction: ", data['Results']['output1'][0])
 
 except urllib.error.HTTPError as error:
     print("The request failed with status code: " + str(error.code))
@@ -73,7 +72,7 @@ except urllib.error.HTTPError as error:
 
 st.markdown(
     """ \n \n
-        Equipe: \n
+        Team class: \n
             Cássio de Paula, \n
             Dilmara Ferreira, \n
             Édson Brilhante, \n
